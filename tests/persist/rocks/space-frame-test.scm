05f438b6ec41fdbe6b8a24b7f90735562e322ba8
Linas Vepstas <linasvepstas@gmail.com>
2022-04-13 16:05:15 -0500

    Extend the unit test for fresh loading.

--- tests/persist/rocks/space-frame-test.scm
+++ tests/persist/rocks/space-frame-test.scm
@@ -35,62 +35,114 @@
 (define mid2-space (cog-new-atomspace mid1-space))
 (define surface-space (cog-new-atomspace mid2-space))
 
-; Splatter some atoms into the various spaces.
-(cog-set-atomspace! base-space)
-(Concept "foo" (ctv 1 0 3))
+(define (setup-and-store)
+	; Splatter some atoms into the various spaces.
+	(cog-set-atomspace! base-space)
+	(Concept "foo" (ctv 1 0 3))
 
-(cog-set-atomspace! mid1-space)
-(Concept "bar" (ctv 1 0 4))
+	(cog-set-atomspace! mid1-space)
+	(Concept "bar" (ctv 1 0 4))
 
-(cog-set-atomspace! mid2-space)
-(ListLink (Concept "foo") (Concept "bar") (ctv 1 0 5))
+	(cog-set-atomspace! mid2-space)
+	(ListLink (Concept "foo") (Concept "bar") (ctv 1 0 5))
 
-(cog-set-atomspace! surface-space)
+	(cog-set-atomspace! surface-space)
 
-; Store the content. Store the Concepts as well as the link,
-; as otherwise, the TV's on the Concepts aren't stored.
-(define storage (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
-(cog-open storage)
-(store-atom (ListLink (Concept "foo") (Concept "bar")))
-(store-atom (Concept "foo"))
-(store-atom (Concept "bar"))
-(cog-close storage)
+	; Store the content. Store the Concepts as well as the link,
+	; as otherwise, the TV's on the Concepts aren't stored.
+	(define storage (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
+	(cog-open storage)
+	(store-atom (ListLink (Concept "foo") (Concept "bar")))
+	(store-atom (Concept "foo"))
+	(store-atom (Concept "bar"))
+	(cog-close storage)
 
-; Clear out the spaces, start with a clean slate.
-(cog-atomspace-clear surface-space)
-(cog-atomspace-clear mid2-space)
-(cog-atomspace-clear mid1-space)
-(cog-atomspace-clear base-space)
+	; Clear out the spaces, start with a clean slate.
+	(cog-atomspace-clear surface-space)
+	(cog-atomspace-clear mid2-space)
+	(cog-atomspace-clear mid1-space)
+	(cog-atomspace-clear base-space)
+)
+
+; Destroy the atomspaces
+(define (zap-spaces)
+	(set! surface-space #f)
+	(set! mid2-space #f)
+	(set! mid1-space #f)
+	(set! base-space #f)
+)
+
+(define (get-cnt ATOM) (inexact->exact (cog-count ATOM)))
 
 ; -------------------------------------------------------------------
 ; Test that deep links are found correctly.
 
+(define (test-deep-link)
+	(setup-and-store)
+
+	; Load everything.
+	(cog-set-atomspace! surface-space)
+	(define storage (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
+	(cog-open storage)
+	(load-atomspace)
+	(cog-close storage)
+
+	; Work on the current surface, but expect to find the deeper ListLink.
+	(define lilly (ListLink (Concept "foo") (Concept "bar")))
+
+	; Verify appropriate atomspace membership
+	(test-equal "link-space" mid2-space (cog-atomspace lilly))
+	(test-equal "foo-space" base-space (cog-atomspace (gar lilly)))
+	(test-equal "bar-space" mid1-space (cog-atomspace (gdr lilly)))
+
+	; Verify appropriate values
+	(test-equal "base-tv" 3 (get-cnt (cog-node 'Concept "foo")))
+	(test-equal "mid1-tv" 4 (get-cnt (cog-node 'Concept "bar")))
+	(test-equal "mid2-tv" 5 (get-cnt lilly))
+)
+
 (define deep-link "test deep links")
 (test-begin deep-link)
-
-; Load everything.
-(cog-set-atomspace! surface-space)
-(set! storage (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
-(cog-open storage)
-(load-atomspace)
-(cog-close storage)
-
-; Work on the current surface, but expect to find the deeper ListLink.
-(define lilly (ListLink (Concept "foo") (Concept "bar")))
-
-; Verify appropriate atomspace membership
-(test-equal "link-space" mid2-space (cog-atomspace lilly))
-(test-equal "foo-space" base-space (cog-atomspace (gar lilly)))
-(test-equal "bar-space" mid1-space (cog-atomspace (gdr lilly)))
-
-; Verify appropriate values
-(test-equal "base-tv" 3 (inexact->exact (cog-count (cog-node 'Concept "foo"))))
-(test-equal "mid1-tv" 4 (inexact->exact (cog-count (cog-node 'Concept "bar"))))
-(test-equal "mid2-tv" 5 (inexact->exact (cog-count lilly)))
-
-(whack "/tmp/cog-rocks-unit-test")
-
+(test-deep-link)
 (test-end deep-link)
 
+; -------------------------------------------------------------------
+; Same as above, except without the pre-existing atomspace hierachty.
+
+(define (test-fresh-link)
+	(setup-and-store)
+	(zap-spaces)
+
+	(define new-base (cog-new-atomspace))
+	(cog-set-atomspace! new-base)
+
+	; Load everything.
+	(set! storage (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
+	(cog-open storage)
+	(load-atomspace)
+	(cog-close storage)
+
+	; Work on the current surface, but expect to find the deeper ListLink.
+	(define lilly (ListLink (Concept "foo") (Concept "bar")))
+
+#! ========
+	; Verify appropriate atomspace membership
+	(test-equal "link-space" mid2-space (cog-atomspace lilly))
+	(test-equal "foo-space" base-space (cog-atomspace (gar lilly)))
+	(test-equal "bar-space" mid1-space (cog-atomspace (gdr lilly)))
+
+	; Verify appropriate values
+	(test-equal "base-tv" 3 (get-cnt (cog-node 'Concept "foo")))
+	(test-equal "mid1-tv" 4 (get-cnt (cog-node 'Concept "bar")))
+	(test-equal "mid2-tv" 5 (get-cnt lilly))
+===== !#
+)
+
+(define fresh-link "test fresh link restore")
+(test-begin fresh-link)
+(test-fresh-link)
+(test-end fresh-link)
+
 ; ===================================================================
+(whack "/tmp/cog-rocks-unit-test")
 (opencog-test-end)
