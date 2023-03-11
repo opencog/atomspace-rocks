450e6e57cb09f78c2f1fa8a9839c01b995ef7e62
Linas Vepstas <linasvepstas@gmail.com>
2023-03-11 13:04:18 -0600

    Start filling in the thread-count unit test

--- tests/persist/rocks/thread-count.scm
+++ tests/persist/rocks/thread-count.scm
@@ -6,6 +6,19 @@
 (use-modules (opencog) (opencog persist))
 (use-modules (opencog persist-rocks))
 
-(define (observe TXT)
-	(define cpt (Concept TXT))
-	
+(define sto (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
+(define open-sto (cog-open sto))
+(define close-sto (cog-close sto))
+
+; Increment and store.
+(define (observe TXTA TXTB)
+	(define ca (Concept TXTA))
+	(define cb (Concept TXTB))
+	(define edge (Edge (Predicate "foo") (List ca cb)))
+	(cog-inc-count! ca 1)
+	(cog-inc-count! cb 1)
+	(cog-inc-count! edge 1)
+	(store-atom ca)
+	(store-atom cb)
+	(store-atom edge)
+)
