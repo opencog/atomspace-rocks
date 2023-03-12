c7033b7f7e4e8eefb1a58bdcbb233a6387ce121e
Linas Vepstas <linasvepstas@gmail.com>
2023-03-12 06:54:05 -0500

    Provide non-racing form of unit test

--- tests/persist/rocks/thread-count.scm
+++ tests/persist/rocks/thread-count.scm
@@ -6,6 +6,8 @@
 (use-modules (opencog) (opencog persist))
 (use-modules (opencog persist-rocks))
 
+(use-modules (ice-9 threads))
+
 (define sto (RocksStorageNode "rocks:///tmp/cog-rocks-unit-test"))
 (define (open-sto) (cog-open sto))
 (define (close-sto) (cog-close sto))
@@ -41,17 +43,29 @@
 	(cog-pop-atomspace)
 )
 
+(define mtx (make-mutex))
+
 ; Fetch, increment and store.
 (define (fetchy TXTA TXTB)
 	(define ca (Concept TXTA))
 	(define cb (Concept TXTB))
 	(define edge (Edge (Predicate "foo") (List ca cb)))
+
+	; Provide a safe fetch that does not race.
+	(if (not (cog-ctv? (cog-tv edge)))
+		(begin
+			(lock-mutex mtx)
+			(if (not (cog-ctv? (cog-tv edge)))
+				(fetch-atom edge))
+			(cog-inc-count! edge 1)
+			(unlock-mutex mtx))
+		(cog-inc-count! edge 1))
+
+	; These will race and ruin the counts.
 	(fetch-atom ca)
 	(fetch-atom cb)
-	(fetch-atom edge)
 	(cog-inc-count! ca 1)
 	(cog-inc-count! cb 1)
-	(cog-inc-count! edge 1)
 	(store-atom ca)
 	(store-atom cb)
 	(store-atom edge)
