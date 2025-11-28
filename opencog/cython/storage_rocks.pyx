db63e089e569d08ec38c1b082985075b46750253
Linas Vepstas <linasvepstas@gmail.com>
2025-11-28 02:05:18 +0000

    Python: regenerate list of types after module load

--- opencog/cython/storage_rocks.pyx
+++ opencog/cython/storage_rocks.pyx
@@ -2,14 +2,15 @@
 # storage_rocks.pyx
 #
 # Defines a python module for the RocksDB storage nodes.
-# There's almost nothing here, just enough to link the C library to 
+# There's almost nothing here, just enough to link the C library to
 # cython so that user can import. Intended use is for the user to say
 #
-# import storage_rocks
+# from opencog.storage_rocks import *
 #
-# and that's all.
+# and that's all. The RocksStorageNode type will then be available.
 #
 
-from opencog.atomspace import types
+from opencog.atomspace import types, regenerate_types
 
-# Great emptiness.
+# Regenerate types so that RocksStorageNode becomes available.
+regenerate_types()
