4bf7ea1f34f3761ab60dd143acadd5609437f6b5
Linas Vepstas <linasvepstas@gmail.com>
2025-11-28 02:22:17 +0000

    Python bindings: add the type constructors.

--- opencog/cython/storage_rocks.pyx
+++ opencog/cython/storage_rocks.pyx
@@ -2,8 +2,7 @@
 # storage_rocks.pyx
 #
 # Defines a python module for the RocksDB storage nodes.
-# There's almost nothing here, just enough to link the C library to
-# cython so that user can import. Intended use is for the user to say
+# Intended use is for the user to say
 #
 # from opencog.storage_rocks import *
 #
@@ -14,3 +13,9 @@ from opencog.atomspace import types, regenerate_types
 
 # Regenerate types so that RocksStorageNode becomes available.
 regenerate_types()
+
+# Import add_node for the type constructors below.
+from opencog.type_ctors import add_node
+
+# Include the auto-generated type constructors.
+include "opencog/persist/rocks-types/persist_rocks_types.pyx"
